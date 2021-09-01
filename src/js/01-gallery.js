9de66bcaa5cae5b8179429be572f48b641b59929
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-09-01 12:35:25 +0300

    updated parcel

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesLibrary = {
+  itemsList: document.querySelector('.gallery'),
+};
+
+function makeGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a> </li>`;
+    })
+    .join('');
+}
+
+imagesLibrary.itemsList.innerHTML = makeGalleryItems(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  disableScroll: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+  loop: true,
+});
+
+// console.log(gallery);
+
+imagesLibrary.itemsList.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  gallery.open('.gallery');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAYER_TIME_ON_EXIT = 'videoplayer-current-time';
+
+const videoPlayer = new Player('vimeo-player');
+
+videoPlayer.on('play', onVideoPlay);
+
+function onVideoPlay() {
+  if (localStorage.getItem(PLAYER_TIME_ON_EXIT)) {
+    videoPlayer.setCurrentTime(localStorage.getItem(PLAYER_TIME_ON_EXIT));
+    player.off('play', onVideoPlay);
+  }
+}
+
+videoPlayer.on('timeupdate', throttle(OnTimeUpdate, 1000));
+
+videoPlayer.on('seeked', OnTimeUpdate);
+
+function OnTimeUpdate() {
+  videoPlayer.getCurrentTime().then(seconds => localStorage.setItem(PLAYER_TIME_ON_EXIT, seconds));
+}
